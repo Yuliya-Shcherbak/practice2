07bc9e4606c766bbc1b9afae1ea40e2b266b1d51
Julia <shcherbak.yuliya@gmail.com>
2016-05-28 20:09:46 +0300

    Initial commit

---
+++ AddressBookLibrary/AddressBookLibrary/AddressBook.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class User
+{
+    public string LastName { get; private set; }
+    public string FirstName { get; private set; }
+    public DateTime Birthdate { get; private set; }
+    public DateTime TimeAdded { get; private set; }
+    public string City { get; private set; }
+    public string Address { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string Gender { get; private set; }
+    public string Email { get; private set; }
+
+    public User(string name, string lastname, DateTime birthDate, DateTime created, string city, string address, string phone, string gender, string email)
+    {
+        FirstName = name;
+        LastName = lastname;
+        Birthdate = birthDate;
+        TimeAdded = created;
+        City = city;
+        Address = address;
+        PhoneNumber = phone;
+        Gender = gender;
+        Email = email;
+    }
+}
+public sealed class AddressBook
+{
+    private static readonly Lazy<AddressBook> lazy = new Lazy<AddressBook>(() => new AddressBook());
+
+    public static AddressBook Instance { get { return lazy.Value; } }
+
+    public delegate void UserAdded(string firstname, string lastname, string act);
+    public delegate void UserRemoved(string firstname, string lastname, string act);
+    public delegate void UserListUsed(string act);
+    public delegate void UserRemoveFail(string firstname, string lastname);
+    public delegate void UserAddFail(string firstname, string lastname);
+
+    public event UserAdded onUserAdd;
+    public event UserRemoved onUserRemove;
+    public event UserListUsed onUserListUse;
+    public event UserRemoveFail onUserRemoveFail;
+    public event UserAddFail onUserAddFail;
+
+    List<User> UserList = new List<User>();
+
+    public void AddUser(string name, string lastname, DateTime birthDate, DateTime created, string city, string address, string phone, string gender, string email)
+    {
+        onUserListUse("add");
+        try
+        {
+            UserList.Add(new User(name, lastname, birthDate, created, city, address, phone, gender, email));
+            onUserAdd(name, lastname, "added");
+        }
+        catch (Exception e)
+        {
+            onUserAddFail(name, lastname);
+        }
+    }
+
+    public void RemoveUser(string name, string lastname, DateTime bithdate)
+    {
+        onUserListUse("remove");
+        try
+        {
+            if (UserList.Remove(UserList.SingleOrDefault(x => x.FirstName == name && x.LastName == lastname && x.Birthdate == bithdate)))
+            {
+                onUserRemove(name, lastname, "removed");
+            }
+            else
+            {
+                Exception ex = new Exception();
+                throw ex;
+            }
+        }
+        catch (Exception e)
+        {
+            onUserRemoveFail(name, lastname);
+        }
+    }
+
+    private AddressBook() { }
+}
+
---
+++ ProgramApp/ProgramApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook aBook = AddressBook.Instance;
+
+            Logger log = new Logger(new PrintToConsole());
+
+            aBook.onUserAdd += log.Info;
+            aBook.onUserAddFail += log.Eror;
+            aBook.onUserListUse += log.Debug;
+            aBook.onUserRemove += log.Info;
+            aBook.onUserRemoveFail += log.Warning;
+
+            aBook.AddUser("John", "Doe", new DateTime(1989, 9, 17), DateTime.Now, "Krivoy Rog", "Katkova street", "224389", "male", "foo@foo.mail.com");
+            aBook.RemoveUser("John", "Lio", new DateTime(1990, 9, 17));
+            aBook.RemoveUser("John", "Doe", new DateTime(1989, 9, 17));
+
+            Console.ReadKey();
+        }
+    }
+}
